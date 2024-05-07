aa0a1c2dc71108edfccd0bdab01a3953a22b8b8e
KhrustDanil <danil.khrust29@gmail.com>
2024-05-07 23:38:10 +0300

    made work

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let numOne = +prompt("Enter first number:");
+let numTwo = +prompt("Enter second number:");
+let numThree = +prompt("Enter third number:");
+
+let average = (numOne + numTwo + numThree) / 3;
+
+alert("Average number:" + average);
